7c8c61072320f4856206c4e3f764d7ca072c1dc2
7Moonday7 <danilchenko.lera@gmail.com>
2021-04-27 12:58:02 +0700

    vue.js integration

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang='ru'>
+<head>
+    <title>Картинная галлерея</title>
+    <meta name='viewport' content='width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0'>
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+<script src="public/js/app.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+//Route::get('/welcome', function () {
+//    return view('welcome');
+//});
 
 Route::get('/', function () {
-    return view('main');
-})->name('app.main');
+    return view('index');
+})->name('app.index');
 
 Route::get('paintings', [PaintingController::class, 'index'])->name('painting.index');
+
+//Auth::routes();
+//
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
