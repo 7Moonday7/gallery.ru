fde7117dfaeb54024579b2d5b7ee38857c2a9a52
7Moonday7 <danilchenko.lera@gmail.com>
2021-04-27 13:45:43 +0700

    nothing

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/welcome', function () {
-//    return view('welcome');
-//});
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Route::get('/', function () {
-    return view('index');
-})->name('app.index');
+    return view('main');
+})->name('app.main');
 
 Route::get('paintings', [PaintingController::class, 'index'])->name('painting.index');
 
-//Auth::routes();
-//
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
